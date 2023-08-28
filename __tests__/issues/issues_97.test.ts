4ddae73a95cc0d6029243dda4efa28812672441a
Bren <verikonos@proton.me>
2023-08-28 14:52:42 -0400

    vitest running __tests__

--- __tests__/issues/issues_97.test.ts
+++ __tests__/issues/issues_97.test.ts
@@ -1,8 +1,13 @@
-import { describe, expect, test } from 'vitest';
+// import { describe, expect, test } from 'vitest';
+import pl from '@polars';
 
 
 describe("issue 97", () => {
 
     test("true is truthy", () => expect(true).toBeTruthy());
+
+    test("???", async () => {
+        console.log('PL', pl);
+    });
 
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import { describe } from 'vitest';
 
 export default defineConfig({
     test: {
-        include: [ "./__tests__/issues/**/*.test.ts" ]
+        include: [ "./__tests__/**/*.test.ts" ],
+        globals: true,
+        setupFiles: [
+            "./__tests__/vitestSetup.ts"
+        ],
+        alias: {
+            "@polars": "./polars/index.ts"
+        }
     }
 });
